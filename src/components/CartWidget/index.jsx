1faef476e385e812c4c2ba823a76ed7ec206b9a3
jlgerbino <joselgerbino@gmail.com>
2022-12-17 08:42:31 -0300

    add items primera entrega

---
+++ src/components/CartWidget/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HiShoppingCart } from "react-icons/hi";
+
+const CartWidget = () => {
+  return (   
+    <HiShoppingCart style={{height: 50, width: 50}}/>    
+  )
+}
+
+export default CartWidget
